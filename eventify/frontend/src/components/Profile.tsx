509561dd0266426401155c258ece1b830e9185ca
zheng yang <teozhengyang15@gmail.com>
2023-06-02 18:23:01 +0800

    add relevant events to profile page

--- eventify/frontend/src/components/Profile.tsx
+++ eventify/frontend/src/components/Profile.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default function Profile() {
   const { authTokens, logoutUser, user } = useContext(AuthContext);
-  const [currUser, setCurrUser] = useState("")
+  const [currUser, setCurrUser] = useState([])
   const [organisedEvents, setOrganisedEvents] = useState([])
   const [participatedEvents, setParticipatedEvents] = useState([])
 
@@ -20,38 +20,51 @@ export default function Profile() {
     const eventsResponse = await axios.get('http://127.0.0.1:8000/api/events/')
     const eventsData = eventsResponse.data
     console.log(eventsData)
-    eventsData.forEach((event) => {
-      if (event.organizers.includes(user.user_id)) {
-        setOrganisedEvents(event)
-        console.log(event)
-      } 
-      if (event.participants.includes(user.user_id)) {
-        setParticipatedEvents(event)
-        console.log(event)
-      } else {
-        // do nothing
-      }
+    const filterOrganiseEvents = eventsData.filter(event => {
+      return event.organizers.includes(user.user_id)
     })
+    setOrganisedEvents(filterOrganiseEvents)
+    const filterParticipateEvents = eventsData.filter(event => {
+      return event.participants.includes(user.user_id)
+    })
+    setParticipatedEvents(filterParticipateEvents)
   };
 
   const organisedEventDiv = (
       <div>
-        <h2>{organisedEvents.name}</h2>
+        {organisedEvents.map((event, i) => (
+          <div key={i}>
+            <h3>Event {i + 1}</h3>
+            <p>Name: {event.name}</p>
+            <p>Description: {event.description}</p>
+            <br />
+          </div>
+        ))}
       </div>
   )
 
   const participatedEventDiv = (
     <div>
-      <h2>{participatedEvents.name}</h2>
-    </div>
+        {participatedEvents.map((event, i) => (
+          <div key={i}>
+            <h3>Event {i + 1}</h3>
+            <p>Name: {event.name}</p>
+            <p>Description: {event.description}</p>
+          </div>
+        ))}
+      </div>
   )
 
   return (
     <div>
       <p>Name: {currUser.first_name} {currUser.last_name}</p>
       <p>Email: {currUser.email}</p>
+      <p> Organised Events </p>
+      <hr />
       {organisedEvents && organisedEventDiv}
       <br/>
+      <p> Participated Events </p>
+      <hr/>
       {participatedEvents && participatedEventDiv}
     </div>
   )
