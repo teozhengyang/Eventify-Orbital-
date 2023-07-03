2aec46360747f63241b20e239e81c7e6d51435d8
LoMaply <linuslee71@gmail.com>
2023-07-03 12:00:17 +0800

    test

--- eventify/frontend/src/components/DisplayMonth.tsx
+++ eventify/frontend/src/components/DisplayMonth.tsx
@@ -2,25 +2,27 @@ import Day from "./Day";
 import { Event } from "src/utils/Types";
 import "/static/css/calendar.css";
 
-
-export default function DisplayMonth({data, eventlist,}: {data: Array<Array<Date>>; eventlist: Array<Event>}) {
+export default function DisplayMonth({data, eventlist}: {data: Array<Array<Date>>; eventlist: Array<Event>}) {
   return (
     <div className="calendar-month" role="month view">
       {data.map((row: Array<Date>, i: number) => (
         <div key={i} className="calendar-week" role="week view">
-          {row.map((day: Date, k: number) => (
-            <Day
-              day={day}
-              key={k}
-              rowIndex={i}
-              events={eventlist.filter((evt) => {
-                const curr = day.valueOf();
-                const start = new Date(evt.start).setHours(0, 0, 0, 0).valueOf();
-                const end = new Date(evt.end).setHours(0, 0, 0, 0).valueOf();
-                return curr >= start && curr <= end;
-              })}
-            />
-          ))}
+          {row.map((day: Date, k: number) => {
+            const filtered = eventlist.filter((evt: Event) => {
+              const curr = day.valueOf();
+              const start = new Date(evt.start).setHours(0, 0, 0, 0).valueOf();
+              const end = new Date(evt.end).setHours(0, 0, 0, 0).valueOf();
+              return curr >= start && curr <= end;
+            })
+            return (
+              <Day
+                day={day}
+                key={k}
+                rowIndex={i}
+                events={filtered}
+              />
+            )
+          })}
         </div>
       ))}
     </div>
